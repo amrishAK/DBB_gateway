97040e7a16c72692f0ac31bc18e959396fb7bdb4
amrishAK <aamrish007@live.com>
2020-03-04 19:18:08 +0000

    implemented dependency injection

---
+++ Manager/RoutingManager.py
@@ -0,0 +1,22 @@
+import requests
+
+class ExampleTest:
+    name = 0
+    def __init__(self):
+        self.name = self.name + 1
+        print("ExampleTEst"+ str(self.name))
+
+class RoutingManager:
+    name = 0
+    def __init__(self,eTest):
+        self.eTest = eTest
+        self.name = self.name + 1
+        print("RoutingManager"+ str(self.name))
+
+    def RedirectRequest(self,headers,rfile):
+        self.eTest.name += 1
+        print("ExampleTEst"+ str(self.eTest.name))
+        content_length = int(headers['Content-Length']) # <--- Gets the size of data
+        post_data = rfile.read(content_length) # <--- Gets the data itself
+        return requests.post(url = "http://localhost:7760/login/login",data=post_data,headers=headers)
+        
--- httpServer.py
+++ httpServer.py
@@ -3,23 +3,30 @@ import socketserver
 from http import server
 import requests
 import json
+from dependency_injector import containers, providers
+from Manager.RoutingManager import RoutingManager
+from Manager.RoutingManager import ExampleTest
 
+#Setting up the Depdency injection container
+class Setup(containers.DynamicContainer):
+    #life time -> like scoped => object is created for each call
+    eTest = providers.Singleton(ExampleTest)   
+    routingManager = providers.Factory(RoutingManager, eTest = eTest)
+    
+#Handler assigned to each incoming Http request
 class SHandler(http.server.BaseHTTPRequestHandler):
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-
-        response = requests.post(url = "http://localhost:7760/login/login",data=post_data,headers=self.headers)
+        router = Setup.routingManager()
+        response = router.RedirectRequest(self.headers,self.rfile)
         self.send_response(response.status_code)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(response.json()).encode('utf-8'))
-    
-    
+
+
 PORT = 8001
 httpd = socketserver.TCPServer(("localhost", PORT), SHandler)
-print( "serving at port", PORT)
 
 try:
     httpd.serve_forever()
