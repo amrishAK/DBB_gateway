d7bddbedf2d42396973119ffc07dbd027b729678
amrishAK <aamrish007@live.com>
2020-04-16 11:52:15 +0100

    updated routes.json

--- Gateway.py
+++ Gateway.py
@@ -5,7 +5,7 @@ from Handler.HttpRequestHandler import HttpRequestHandler
 
 
 def run():
-    httpd = socketserver.TCPServer(("0.0.0.0", 8080), HttpRequestHandler)
+    httpd = socketserver.TCPServer(("0.0.0.0", 8081), HttpRequestHandler)
     try:
         httpd.serve_forever()
     except :
