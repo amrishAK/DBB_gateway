236fb1481d63e3be83e7cde972ea4e63d8b861f1
amrishAK <aamrish007@live.com>
2020-04-16 09:51:04 +0100

    port updated

--- Gateway.py
+++ Gateway.py
@@ -5,7 +5,7 @@ from Handler.HttpRequestHandler import HttpRequestHandler
 
 
 def run():
-    httpd = socketserver.TCPServer(("localhost", 8001), HttpRequestHandler)
+    httpd = socketserver.TCPServer(("localhost", 8080), HttpRequestHandler)
     try:
         httpd.serve_forever()
     except :
